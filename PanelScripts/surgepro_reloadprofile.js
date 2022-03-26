ad18b82fbb0b3ece78a26c5bdb552fb9e40bff6e
guyuqiang <1243952362@qq.com>
2022-03-26 15:56:22 +0800

    init

--- PanelScripts/surgepro_reloadprofile.js
+++ PanelScripts/surgepro_reloadprofile.js
@@ -2,10 +2,10 @@ let params = getParams($argument)
 
 !(async () => {
     /* 时间获取 */
-    let traffic = (await httpAPI("/v1/traffic", "GET"))
-    let dateNow = new Date()
-    let dateTime = Math.floor(traffic.startTime * 1000)
-    let startTime = timeTransform(dateNow, dateTime)
+    let traffic = (await httpAPI("/v1/traffic", "GET"));
+    let dateNow = new Date();
+    let dateTime = Math.floor(traffic.startTime * 1000);
+    let startTime = timeTransform(dateNow, dateTime);
 
     if ($trigger === "button") {
         await httpAPI("/v1/profiles/reload");
@@ -23,24 +23,24 @@ let params = getParams($argument)
 function timeTransform(dateNow, dateTime) {
     let dateDiff = dateNow - dateTime;
     // 计算出相差天数
-    let days = Math.floor(dateDiff / (24 * 3600 * 1000))
+    let days = Math.floor(dateDiff / (24 * 3600 * 1000));
     // 计算天数后剩余的毫秒数
-    let leave1 = dateDiff % (24 * 3600 * 1000)
+    let leave1 = dateDiff % (24 * 3600 * 1000);
     // 计算出小时数
-    let hours = Math.floor(leave1 / (3600 * 1000))
+    let hours = Math.floor(leave1 / (3600 * 1000));
     // 计算相差分钟数
-    let leave2 = leave1 % (3600 * 1000)
-    let minutes = Math.floor(leave2 / (60 * 1000))
+    let leave2 = leave1 % (3600 * 1000);
+    let minutes = Math.floor(leave2 / (60 * 1000));
     // 计算相差秒数
-    let leave3 = leave2 % (60 * 1000)
-    let seconds = Math.round(leave3 / 1000)
+    let leave3 = leave2 % (60 * 1000);
+    let seconds = Math.round(leave3 / 1000);
 
-    seconds = seconds < 10 ? ('0' + seconds) : seconds
-    minutes = minutes < 10 ? ('0' + minutes) : minutes
-    hours = days * 24 + hours
-    hours = hours < 10 ? ('0' + hours) : hours
+    seconds = seconds < 10 ? ('0' + seconds) : seconds;
+    minutes = minutes < 10 ? ('0' + minutes) : minutes;
+    hours = days * 24 + hours;
+    hours = hours < 10 ? ('0' + hours) : hours;
 
-    return `${hours}:${minutes}:${seconds}`
+    return `${hours}:${minutes}:${seconds}`;
 }
 
 
